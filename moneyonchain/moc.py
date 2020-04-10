c28face28225b31016786152db24a3e1729b8b14
mulonemartin <mulone.martin@gmail.com>
2020-04-10 11:21:24 -0300

    MoC State

---
+++ moneyonchain/moc.py
@@ -0,0 +1,51 @@
+"""
+                    GNU AFFERO GENERAL PUBLIC LICENSE
+                       Version 3, 19 November 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+ THIS IS A PART OF MONEY ON CHAIN PACKAGE
+ by Martin Mulone (martin.mulone@moneyonchain.com)
+
+"""
+
+import os
+import logging
+from web3 import Web3
+
+from moneyonchain.contract import Contract
+
+
+class MoCState(Contract):
+    log = logging.getLogger()
+
+    contract_abi = Contract.content_abi_file(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), 'abi/MoCState.abi'))
+    contract_bin = Contract.content_bin_file(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), 'abi/MoCState.bin'))
+
+    def __init__(self, connection_manager, contract_address=None, contract_abi=None, contract_bin=None):
+
+        if not contract_address:
+            # load from connection manager
+            network = connection_manager.network
+            contract_address = connection_manager.options['networks'][network]['addresses']['MoCState']
+
+        super().__init__(connection_manager,
+                         contract_address=contract_address,
+                         contract_abi=contract_abi,
+                         contract_bin=contract_bin)
+
+        # finally load the contract
+        self.load_contract()
+
+    def bitcoin_price(self, formatted=True):
+        """Bitcoin price in USD"""
+
+        price = self.sc.functions.getBitcoinPrice().call()
+        if formatted:
+            price = Web3.fromWei(price, 'ether')
+
+        return price
