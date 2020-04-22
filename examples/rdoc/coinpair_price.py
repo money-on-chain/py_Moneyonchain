1e11435fa0bc79365de1f4cbb3619186708cfbb6
mulonemartin <mulone.martin@gmail.com>
2020-04-22 17:43:03 -0300

    Coin pair price

---
+++ examples/rdoc/coinpair_price.py
@@ -0,0 +1,15 @@
+"""
+Coin pair price
+"""
+
+from moneyonchain.manager import ConnectionManager
+from moneyonchain.oracle import CoinPairPrice
+
+
+network = 'rdocTestnetAlpha'
+connection_manager = ConnectionManager(network=network)
+print("Connecting to %s..." % network)
+print("Connected: {conectado}".format(conectado=connection_manager.is_connected))
+
+contract = CoinPairPrice(connection_manager)
+print(contract.price())
