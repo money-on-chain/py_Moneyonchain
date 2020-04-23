ab40ad5aed7f6be3da80db1021047cdf6c6be179
mulonemartin <mulone.martin@gmail.com>
2020-04-23 16:02:05 -0300

    Dex

---
+++ examples/dex/token_pairs.py
@@ -0,0 +1,12 @@
+from moneyonchain.manager import ConnectionManager
+from moneyonchain.dex import MoCDecentralizedExchange
+
+
+network = 'dexTestnet'
+connection_manager = ConnectionManager(network=network)
+print("Connecting to %s..." % network)
+print("Connected: {conectado}".format(conectado=connection_manager.is_connected))
+
+print("Connecting to MoCDecentralizedExchange")
+dex = MoCDecentralizedExchange(connection_manager)
+print(dex.token_pairs())
