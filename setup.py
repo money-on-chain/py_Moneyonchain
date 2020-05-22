67d6e219852538d7a7c3fd69f3fb6186e00c062c
mulonemartin <mulone.martin@gmail.com>
2020-05-22 10:27:32 -0300

    bump 0.0.21

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='moneyonchain',
-    version='0.0.20',
+    version='0.0.21',
     packages=['moneyonchain'],
     url='https://github.com/moneyonchain/py_Moneyonchain/',
     author='Martin Mulone',
