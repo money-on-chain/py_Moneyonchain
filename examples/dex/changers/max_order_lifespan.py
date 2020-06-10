bb6cdc424414998bc3da75cc2eed7d94ac907b58
mulonemartin <mulone.martin@gmail.com>
2020-06-10 16:05:06 -0300

    Max Order Lifespan

---
+++ examples/dex/changers/max_order_lifespan.py
@@ -0,0 +1,35 @@
+"""
+Changer Max Order lifespan
+"""
+
+from moneyonchain.manager import ConnectionManager
+from moneyonchain.changers import DexMaxOrderLifespanChanger
+
+import logging
+import logging.config
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+log = logging.getLogger('default')
+
+network = 'dexTestnet'
+connection_manager = ConnectionManager(network=network)
+print("Connecting to %s..." % network)
+print("Connected: {conectado}".format(conectado=connection_manager.is_connected))
+
+contract = DexMaxOrderLifespanChanger(connection_manager)
+
+max_order_life_span = 2400
+
+tx_hash, tx_receipt = contract.constructor(max_order_life_span,
+                                           execute_change=False)
+if tx_receipt:
+    print("Changer Contract Address: {address}".format(address=tx_receipt.contractAddress))
+else:
+    print("Error deploying changer")
+
+"""
+
+
+"""
