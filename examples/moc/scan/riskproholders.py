065ce746e1da8fd2651cbc9c473a96e01c4b4b7c
mulonemartin <mulone.martin@gmail.com>
2020-07-03 09:21:59 -0300

    Risk pro holders payment event

---
+++ examples/moc/scan/riskproholders.py
@@ -0,0 +1,54 @@
+"""
+
+Get events RiskProHoldersInterestPay from mocinrate
+
+"""
+
+import time
+import csv
+
+from moneyonchain.manager import ConnectionManager
+from moneyonchain.moc import MoCInrate
+from moneyonchain.events import MoCInrateRiskProHoldersInterestPay
+
+network = 'mocMainnet2'
+connection_manager = ConnectionManager(network=network)
+print("Connecting to %s..." % network)
+print("Connected: {conectado}".format(conectado=connection_manager.is_connected))
+
+print("Starting to import events from contract...")
+start_time = time.time()
+
+moc_inrate = MoCInrate(connection_manager)
+
+events_functions = ['RiskProHoldersInterestPay']
+hours_delta = 0
+from_block = 2377097  # from block start
+to_block = 2463497  # block end or 0 to last block
+l_events = moc_inrate.logs_from(events_functions, from_block, to_block, block_steps=2880)
+
+l_historic_data = list()
+if 'RiskProHoldersInterestPay' in l_events:
+    if l_events['RiskProHoldersInterestPay']:
+        count = 0
+        for e_event_block in l_events['RiskProHoldersInterestPay']:
+            for e_event in e_event_block:
+                tx_event = MoCInrateRiskProHoldersInterestPay(connection_manager, e_event)
+                l_historic_data.append(tx_event.row())
+
+# Write list to CSV File
+
+if l_historic_data:
+    columns = MoCInrateRiskProHoldersInterestPay.columns()
+    path_file = '{0}_riskpro_holders_payments_{1}_{2}.csv'.format(network, from_block, to_block)
+    with open(path_file, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(columns)
+
+        count = 0
+        for historic_data in l_historic_data:
+            count += 1
+            writer.writerow(historic_data)
+
+duration = time.time() - start_time
+print("Getting events from MOC done! Succesfull!! Done in {0} seconds".format(duration))
