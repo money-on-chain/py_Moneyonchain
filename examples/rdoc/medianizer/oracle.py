76134c6458e4cd05b6b0b233c7ce2ad9e64e0e14
mulonemartin <mulone.martin@gmail.com>
2020-07-10 11:20:39 -0300

    Add get price provider address to moc state

--- examples/rdoc/medianizer/oracle.py
+++ examples/rdoc/medianizer/oracle.py
@@ -18,8 +18,9 @@ log.info("Connected: {conectado}".format(conectado=connection_manager.is_connect
 
 #oracle_address = '0x2B54819531B7126bDEE2CeFDD9c5342d6c307595'
 #oracle_address = '0x01a165cC33Ff8Bd0457377379962232886be3DE6'
-oracle_address = '0x9d4b2c05818A0086e641437fcb64ab6098c7BbEc'
-oracle = RDOCMoCMedianizer(connection_manager,
-                           contract_address=oracle_address)
+#oracle_address = '0x9d4b2c05818A0086e641437fcb64ab6098c7BbEc'
+#oracle_address = '0x9315AFD6aEc0bb1C1FB3fdcdC2E43797B0A61853'
+oracle_address = '0xb856Ca7c722cfb202D81c55DC7925e02ed3f0A2F'
+oracle = RDOCMoCMedianizer(connection_manager, contract_address=oracle_address) #contract_address=oracle_address
 #print(oracle.price())
 print(oracle.peek())
--- examples/rdoc/medianizer/price_feed_verification.py
+++ examples/rdoc/medianizer/price_feed_verification.py
@@ -23,20 +23,38 @@ log.info("Connected: {conectado}".format(conectado=connection_manager.is_connect
 
 
 oracle_address = '0x504EfCadFB020d6bBaeC8a5c5BB21453719d0E00'
-feeder_address = '0xBEd51D83CC4676660e3fc3819dfAD8238549B975'
+feeder_address_1 = '0x461750b4824b14c3d9b7702bC6fBB82469082b23'
+feeder_address_2 = '0xBEd51D83CC4676660e3fc3819dfAD8238549B975'
 
 oracle = RDOCMoCMedianizer(connection_manager,
                            contract_address=oracle_address)
 
-feeder = RDOCPriceFeed(connection_manager,
-                       contract_address=feeder_address,
-                       contract_address_moc_medianizer=oracle_address)
+feeder_1 = RDOCPriceFeed(connection_manager,
+                         contract_address=feeder_address_1,
+                         contract_address_moc_medianizer=oracle_address)
 
 print("Oracle price:")
 print(oracle.peek())
 
+print("Price Feeder 1")
+print("===============")
+
 print("Price feeder price and have value (if have value if false, no price setted) :")
-print(feeder.peek())
+print(feeder_1.peek())
 
 print("Index > 0 is active price feeder")
-print(oracle.indexes(feeder_address))
+print(oracle.indexes(feeder_address_1))
+
+
+feeder_2 = RDOCPriceFeed(connection_manager,
+                         contract_address=feeder_address_2,
+                         contract_address_moc_medianizer=oracle_address)
+
+print("Price Feeder 2")
+print("===============")
+
+print("Price feeder price and have value (if have value if false, no price setted) :")
+print(feeder_2.peek())
+
+print("Index > 0 is active price feeder")
+print(oracle.indexes(feeder_address_2))
