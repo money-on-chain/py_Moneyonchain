44ea6d98befc56bcb4df9eb7c55d236bf68aa45f
mulonemartin <mulone.martin@gmail.com>
2020-09-11 10:11:10 -0300

    examples prices and oracle

--- examples/moc/medianizer/oracle_price.py
+++ examples/moc/medianizer/oracle_price.py
@@ -1,9 +1,10 @@
 """
-Prices in enviroments from MOC
+Oracle price get current oracle from MOC
 """
 
 from moneyonchain.manager import ConnectionManager
 from moneyonchain.moc import MoC
+from moneyonchain.moc import MoCMedianizer
 
 # Network types
 #
@@ -19,5 +20,9 @@ print("Connected: {conectado}".format(conectado=connection_manager.is_connected)
 print("Connecting to MoC Main Contract")
 moc_contract = MoC(connection_manager)
 
-print("Bitcoin Price in USD: {0}".format(moc_contract.sc_moc_state.bitcoin_price()))
-print("Bitcoin Moving Average in USD: {0}".format(moc_contract.sc_moc_state.bitcoin_moving_average()))
+oracle_provider = moc_contract.sc_moc_state.price_provider()
+print("Oracle address: {0}".format(oracle_provider))
+
+oracle = MoCMedianizer(connection_manager, contract_address=oracle_provider)
+
+print("Bitcoin Price in USD: {0}".format(oracle.price()))
--- examples/rdoc/medianizer/oracle_price.py
+++ examples/rdoc/medianizer/oracle_price.py
@@ -1,9 +1,10 @@
 """
-Prices in enviroments from RDOC
+Oracle price get current oracle from MOC
 """
 
 from moneyonchain.manager import ConnectionManager
 from moneyonchain.rdoc import RDOCMoC
+from moneyonchain.rdoc import RDOCMoCMedianizer
 
 # Network types
 #
@@ -19,5 +20,9 @@ print("Connected: {conectado}".format(conectado=connection_manager.is_connected)
 print("Connecting to RDOC Main Contract")
 moc_contract = RDOCMoC(connection_manager)
 
-print("RIF Price in USD: {0}".format(moc_contract.sc_moc_state.bitcoin_price()))
-print("RIF Moving Average in USD: {0}".format(moc_contract.sc_moc_state.bitcoin_moving_average()))
+oracle_provider = moc_contract.sc_moc_state.price_provider()
+print("Oracle address: {0}".format(oracle_provider))
+
+oracle = RDOCMoCMedianizer(connection_manager, contract_address=oracle_provider)
+
+print("RIF Price in USD: {0}".format(oracle.price()))
