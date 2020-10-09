e25e6746535f543ca5381b33060c5ac493bdf8ee
mulonemartin <mulone.martin@gmail.com>
2020-10-09 15:51:37 -0300

    Added splitter moc-testnet

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='moneyonchain',
-    version='0.0.48',
+    version='0.0.49',
     packages=['moneyonchain'],
     url='https://github.com/moneyonchain/py_Moneyonchain/',
     author='Martin Mulone',
