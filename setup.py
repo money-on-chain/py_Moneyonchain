38de255098fefd3ee9ace1e3a92b639cb4198294
shanshi <yamilacariati@hotmail.com>
2020-11-02 16:34:09 +0000

    Change library version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='moneyonchain',
-    version='0.0.50',
+    version='0.0.53',
     packages=['moneyonchain'],
     url='https://github.com/moneyonchain/py_Moneyonchain/',
     author='Martin Mulone',
