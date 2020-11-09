7fa12a867f67f1f0dec26ec9baa95e2102c4d888
mulonemartin <mulone.martin@gmail.com>
2020-11-09 15:31:57 -0300

    example stoppable

---
+++ examples/rdoc/governance/stoppable.py
@@ -0,0 +1,25 @@
+from moneyonchain.manager import ConnectionManager
+from moneyonchain.governance import RDOCStopper
+from moneyonchain.rdoc import RDOCMoC
+
+import logging
+import logging.config
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+log = logging.getLogger('default')
+
+
+network = 'rdocMainnet'
+connection_manager = ConnectionManager(network=network)
+print("Connecting to %s..." % network)
+print("Connected: {conectado}".format(conectado=connection_manager.is_connected))
+
+contract_moc = RDOCMoC(connection_manager)
+contract_stopper = RDOCStopper(connection_manager)
+
+print("Paused: {0}".format(contract_moc.paused()))
+print("Stoppable: {0}".format(contract_moc.stoppable()))
+print("Stopper: {0}".format(contract_moc.stopper()))
+print("Owner: {0}".format(contract_stopper.owner()))
---
+++ examples/rdoc/governance/unpause.py
@@ -0,0 +1,39 @@
+"""
+
+This unpause MOC Contract
+
+"""
+
+from moneyonchain.manager import ConnectionManager
+from moneyonchain.governance import RDOCStopper
+from moneyonchain.rdoc import RDOCMoC
+
+import logging
+import logging.config
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+log = logging.getLogger('default')
+
+
+network = 'rdocMainnet'
+connection_manager = ConnectionManager(network=network)
+print("Connecting to %s..." % network)
+print("Connected: {conectado}".format(conectado=connection_manager.is_connected))
+
+
+contract_moc = RDOCMoC(connection_manager)
+contract_stopper = RDOCStopper(connection_manager)
+
+contract_to_pause = contract_moc.address()
+tx_hash, tx_receipt = contract_stopper.unpause(contract_to_pause)
+if tx_receipt:
+    print("Stop Contract Address: {address} successfully!".format(address=contract_to_pause))
+else:
+    print("Error Stopping contract")
+
+
+"""
+
+"""
