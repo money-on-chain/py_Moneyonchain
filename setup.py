1f24cc145ef18ef25182e1806c7e3f71459e7bb6
shanshi <yamilacariati@hotmail.com>
2021-01-12 13:49:40 +0000

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='moneyonchain',
-    version='0.1.0',
+    version='1.0.0',
     packages=['moneyonchain'],
     url='https://github.com/moneyonchain/py_Moneyonchain/',
     author='Martin Mulone',
