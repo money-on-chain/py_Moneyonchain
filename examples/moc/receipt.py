4ac658a203b51889b94852ad73f75c57b9b34e06
mulonemartin <mulone.martin@gmail.com>
2021-02-04 11:17:40 -0300

    debug_transactions enabled

--- examples/moc/receipt.py
+++ examples/moc/receipt.py
@@ -1,12 +1,14 @@
 """
 """
 
+from web3 import Web3
+
 from moneyonchain.networks import NetworkManager
 from moneyonchain.transaction import TransactionReceiptBase
 
 from brownie import web3
 
-connection_network = 'rskTesnetLocal'
+connection_network = 'rskTesnetPublic'
 config_network = 'mocTestnetAlpha'
 
 # init network manager
@@ -30,11 +32,27 @@ print(tx_receipt)
 tx_receipt = TransactionReceiptBase(tx_id)
 tx_receipt.info()
 
-tx_id = '0x73a0ce47ab64a31e542670578c73dbfb8f21c1c59a176bd462918db63566f42a'
-tx_receipt = TransactionReceiptBase(tx_id, trace_enabled=True)
-print(tx_receipt.status)
-#tx_receipt.info()
-print(tx_receipt.revert_msg)
+# tx_id = '0x73a0ce47ab64a31e542670578c73dbfb8f21c1c59a176bd462918db63566f42a'
+# tx_receipt = TransactionReceiptBase(tx_id, trace_enabled=True)
+# print(tx_receipt.status)
+# #tx_receipt.info()
+# print(tx_receipt.revert_msg)
+
+
+tx_hash = tx_receipt.txid
+print(tx_hash)
+
+print(tx_receipt.events)
+print(tx_receipt.logs)
+print(tx_receipt.sender)
+print(tx_receipt.receiver)
+print(tx_receipt.fn_name)
+print(tx_receipt.contract_name)
+print(tx_receipt.value)
+print(tx_receipt.gas_price)
+print(tx_receipt.confirmations)
+
+
 
 # finally disconnect from network
 network_manager.disconnect()
