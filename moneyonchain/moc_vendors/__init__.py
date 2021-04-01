e0230ff322cc0f844001a226addd3a4b23aa0ff4
mulonemartin <mulone.martin@gmail.com>
2021-04-01 17:06:25 -0300

    MoC Inrate commission changer

--- moneyonchain/moc_vendors/__init__.py
+++ moneyonchain/moc_vendors/__init__.py
@@ -14,4 +14,5 @@ from .events import MoCExchangeRiskProMint, MoCExchangeRiskProWithDiscountMint, 
     MoCStateMoCTokenChanged, MoCStateMoCVendorsChanged, \
     MoCVendorsVendorRegistered, MoCVendorsVendorUpdated, MoCVendorsVendorUnregistered, \
     MoCVendorsVendorStakeAdded, MoCVendorsVendorStakeRemoved, MoCVendorsTotalPaidInMoCReset, MoCContractLiquidated
-from .changers import MoCStateMoCPriceProviderChanger, MoCStateLiquidationEnabledChanger, MoCVendorsChanger
+from .changers import MoCStateMoCPriceProviderChanger, MoCStateLiquidationEnabledChanger, MoCVendorsChanger, \
+    MoCInrateCommissionsChanger
--- moneyonchain/moc_vendors/changers.py
+++ moneyonchain/moc_vendors/changers.py
@@ -120,3 +120,37 @@ class MoCVendorsChanger(BaseChanger):
             self.log.info("Change successfull!")
 
         return tx_receipt
+
+
+class MoCInrateCommissionsChanger(BaseChanger):
+    contract_name = 'MoCInrateCommissionsChanger'
+
+    contract_abi = ContractBase.content_abi_file(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), 'abi/MoCInrateCommissionsChanger.abi'))
+    contract_bin = ContractBase.content_bin_file(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), 'abi/MoCInrateCommissionsChanger.bin'))
+
+    mode = 'MoC'
+
+    def constructor(self, commission_rates, execute_change=False, **tx_arguments):
+
+        config_network = self.network_manager.config_network
+        contract_address = self.network_manager.options['networks'][config_network]['addresses']['MoCInrate']
+
+        self.log.info("Deploying new contract...")
+
+        tx_receipt = self.deploy(contract_address, commission_rates, **tx_arguments)
+
+        tx_receipt.info()
+        tx_receipt.info_to_log()
+
+        self.log.info("Deployed contract done!")
+        self.log.info("Changer Contract Address: {address}".format(address=tx_receipt.contract_address))
+
+        if execute_change:
+            self.log.info("Executing change....")
+            governor = Governor(self.network_manager).from_abi()
+            tx_receipt = governor.execute_change(tx_receipt.contract_address, **tx_arguments)
+            self.log.info("Change successfull!")
+
+        return tx_receipt
