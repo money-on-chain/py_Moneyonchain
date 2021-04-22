628a821f5b005885d4475b302c74999c776f4cbb
mulonemartin <mulone.martin@gmail.com>
2021-04-22 14:15:57 -0300

    Upgrader changer verification

--- moneyonchain/governance/__init__.py
+++ moneyonchain/governance/__init__.py
@@ -2,4 +2,4 @@ from .admin import ProxyAdmin
 from .governed import Governed, RDOCGoverned
 from .governor import Governor, DEXGovernor, RDOCGovernor
 from .stopper import MoCStopper, RDOCStopper
-from .changers import RDOCUpgraderChanger
+from .changers import UpgraderChanger, RDOCUpgraderChanger
--- moneyonchain/governance/changers.py
+++ moneyonchain/governance/changers.py
@@ -18,6 +18,55 @@ from web3.types import BlockIdentifier
 from moneyonchain.contract import ContractBase
 
 
+class UpgraderChanger(ContractBase):
+    contract_name = 'UpgraderChanger'
+
+    contract_abi = ContractBase.content_abi_file(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), 'abi/UpgraderChanger.abi'))
+    contract_bin = ContractBase.content_bin_file(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), 'abi/UpgraderChanger.bin'))
+
+    mode = 'MoC'
+    precision = 10 ** 18
+
+    def __init__(self,
+                 network_manager,
+                 contract_name=None,
+                 contract_address=None,
+                 contract_abi=None,
+                 contract_bin=None):
+        config_network = network_manager.config_network
+        if not contract_address:
+            raise ValueError("You need to pass contract address")
+
+        super().__init__(network_manager,
+                         contract_name=contract_name,
+                         contract_address=contract_address,
+                         contract_abi=contract_abi,
+                         contract_bin=contract_bin)
+
+    def new_implementation(self, block_identifier: BlockIdentifier = 'latest'):
+        """Contract address output"""
+
+        result = self.sc.newImplementation(block_identifier=block_identifier)
+
+        return result
+
+    def proxy(self, block_identifier: BlockIdentifier = 'latest'):
+        """Contract address output"""
+
+        result = self.sc.proxy(block_identifier=block_identifier)
+
+        return result
+
+    def upgrade_delegator(self, block_identifier: BlockIdentifier = 'latest'):
+        """Contract address output"""
+
+        result = self.sc.upgradeDelegator(block_identifier=block_identifier)
+
+        return result
+
+
 class RDOCUpgraderChanger(ContractBase):
     contract_name = 'RDOCUpgraderChanger'
 
