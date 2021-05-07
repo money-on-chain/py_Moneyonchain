2d97c500f5b45d48a16f49f3101fe39a347efe8c
mulonemartin <mulone.martin@gmail.com>
2021-05-07 16:03:42 -0300

    Proxy oracle

--- moneyonchain/medianizer/__init__.py
+++ moneyonchain/medianizer/__init__.py
@@ -3,6 +3,7 @@ from .feedfactory import FeedFactory, RRC20FeedFactory, RDOCFeedFactory, \
 from .medianizer import MoCMedianizer, RRC20MoCMedianizer, RDOCMoCMedianizer, \
     ETHMoCMedianizer, ProxyMoCMedianizer
 from .pricefeed import PriceFeed, RRC20PriceFeed, RDOCPriceFeed, ETHPriceFeed
+from .events import EventCreated
 from .changers import PriceFeederWhitelistChanger, \
     RDOCPriceFeederWhitelistChanger, \
     PriceFeederAdderChanger, \
