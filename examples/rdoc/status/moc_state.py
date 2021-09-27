ae034482e3f3980c2be7f11ad7a6f6f31b7d5516
mulonemartin <mulone.martin@gmail.com>
2021-09-27 14:48:16 -0300

    Update brownie

--- examples/rdoc/status/moc_state.py
+++ examples/rdoc/status/moc_state.py
@@ -2,14 +2,22 @@ from moneyonchain.networks import network_manager
 from moneyonchain.rdoc import RDOCMoCState
 
 
-connection_network = 'rskTestnetPublic'
-config_network = 'rdocTestnetAlpha'
+connection_network = 'rskMainnetPublic'
+config_network = 'rdocMainnet'
+
 
 
 # Connect to network
 network_manager.connect(connection_network=connection_network, config_network=config_network)
 
 moc_state = RDOCMoCState(network_manager).from_abi()
+
+bucket_x2 = moc_state.bucket_x2()
+print(bucket_x2)
+
+bucket_c0 = moc_state.bucket_c0()
+print(bucket_c0)
+
 
 print("State: {0}".format(moc_state.state()))
 
@@ -21,11 +29,11 @@ print("Cobj X2: {0}".format(moc_state.cobj_X2()))
 #print("Max mint riskpro avail: {0}".format(moc_state.max_mint_bpro_available()))
 #print("Max mint riskpro: {0}".format(moc_state.max_mint_bpro()))
 print("Absolute max doc: {0}".format(moc_state.absolute_max_doc()))
-print("Max RISKPROx: {0}".format(moc_state.max_bprox(str.encode('X2'))))
+print("Max RISKPROx: {0}".format(moc_state.max_bprox(bucket_x2)))
 #print("Max RISKPROx btc value: {0}".format(moc_state.max_bprox_btc_value()))
 print("Absolute max bpro: {0}".format(moc_state.absolute_max_bpro()))
 print("Free doc: {0}".format(moc_state.free_doc()))
-print("Leverage: {0}".format(moc_state.leverage(str.encode('X2'))))
+print("Leverage: {0}".format(moc_state.leverage(bucket_x2)))
 
 
 print("RIF Price in USD: {0}".format(moc_state.bitcoin_price()))
@@ -45,20 +53,20 @@ print("RIFP Tec Price: {0}".format(moc_state.bpro_tec_price()))
 print("RISKPROX Price: {0}".format(moc_state.bprox_price()))
 print("RISKPROX Tec Price: {0}".format(moc_state.btc2x_tec_price()))
 
-print("Inrate bag: {0}".format(moc_state.get_inrate_bag(str.encode('X2'))))
+print("Inrate bag: {0}".format(moc_state.get_inrate_bag(bucket_x2)))
 
 
 print("X2")
-print("Bucket NBTC: {0}".format(moc_state.bucket_nbtc(str.encode('X2'))))
-print("Bucket NDOC: {0}".format(moc_state.bucket_ndoc(str.encode('X2'))))
-print("Bucket NBPRO: {0}".format(moc_state.bucket_nbpro(str.encode('X2'))))
-print("Coverage RISKPROX: {0}".format(moc_state.coverage(str.encode('X2'))))
+print("Bucket NBTC: {0}".format(moc_state.bucket_nbtc(bucket_x2)))
+print("Bucket NDOC: {0}".format(moc_state.bucket_ndoc(bucket_x2)))
+print("Bucket NBPRO: {0}".format(moc_state.bucket_nbpro(bucket_x2)))
+print("Coverage RISKPROX: {0}".format(moc_state.coverage(bucket_x2)))
 
 print("C0")
-print("Bucket NBTC: {0}".format(moc_state.bucket_nbtc(str.encode('C0'))))
-print("Bucket NDOC: {0}".format(moc_state.bucket_ndoc(str.encode('C0'))))
-print("Bucket NBPRO: {0}".format(moc_state.bucket_nbpro(str.encode('C0'))))
-print("Coverage RISKPROX: {0}".format(moc_state.coverage(str.encode('C0'))))
+print("Bucket NBTC: {0}".format(moc_state.bucket_nbtc(bucket_c0)))
+print("Bucket NDOC: {0}".format(moc_state.bucket_ndoc(bucket_c0)))
+print("Bucket NBPRO: {0}".format(moc_state.bucket_nbpro(bucket_c0)))
+print("Coverage RISKPRO: {0}".format(moc_state.coverage(bucket_c0)))
 
 
 print("Is liquidation: {0}".format(moc_state.is_liquidation()))
@@ -75,9 +83,9 @@ print("Current_abundance_ratio: {0}".format(moc_state.current_abundance_ratio())
 print("abundance_ratio: {0}".format(moc_state.abundance_ratio(int(2.242807702008664948*10*18)))) #, block_identifier=1233780
 
 
-print("Bucket NDOC: {0}".format(moc_state.bucket_ndoc(str.encode('C0'), formatted=False)))
+print("Bucket NDOC: {0}".format(moc_state.bucket_ndoc(bucket_c0, formatted=False)))
 print("RDOC Totaly: {0}".format(moc_state.doc_total_supply(formatted=False)))
-print("Bucket NBTC: {0}".format(moc_state.bucket_nbtc(str.encode('C0'), formatted=False)))
+print("Bucket NBTC: {0}".format(moc_state.bucket_nbtc(bucket_c0, formatted=False)))
 print("RIF in sysm: {0}".format(moc_state.rbtc_in_system(formatted=False)))
 
 
