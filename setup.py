68eb5c34dbc8364e657626bae4a491a1df41d1d5
mulonemartin <mulone.martin@gmail.com>
2022-06-29 16:56:55 -0300

    Dave fixes to new web3py releases

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='moneyonchain',
-    version='0.0.53',
+    version='0.0.55',
     packages=['moneyonchain'],
     url='https://github.com/moneyonchain/py_Moneyonchain/',
     author='Martin Mulone',
