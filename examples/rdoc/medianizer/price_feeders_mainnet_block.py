874b8d53309234944466db77a546f3c771b16bf1
Juan S. Bokser <juan.bokser@gmail.com>
2023-05-09 17:51:37 -0300

    update price_feeders_mainnet_block.py

--- examples/rdoc/medianizer/price_feeders_mainnet_block.py
+++ examples/rdoc/medianizer/price_feeders_mainnet_block.py
@@ -3,6 +3,7 @@ Price feeder verification. Test if pricefeeder is working and sending prices.
 """
 import datetime
 from tabulate import tabulate
+from decimal import Decimal
 
 from moneyonchain.networks import network_manager
 from moneyonchain.medianizer import RDOCMoCMedianizer, \
@@ -28,7 +29,7 @@ network_manager.connect(connection_network=connection_network, config_network=co
 
 oracle_address = '0x504EfCadFB020d6bBaeC8a5c5BB21453719d0E00'
 feeders = [('0x461750b4824b14c3d9b7702bC6fBB82469082b23', '# MOC'),
-           ('0xBEd51D83CC4676660e3fc3819dfAD8238549B975', '# RSK')]
+           ('0xBEd51D83CC4676660e3fc3819dfAD8238549B975', '# ROC')]
 
 oracle = RDOCMoCMedianizer(network_manager,
                            contract_address=oracle_address).from_abi()
@@ -37,15 +38,19 @@ feeder_moc = RDOCPriceFeed(network_manager,
                            contract_address=feeders[0][0],
                            contract_address_moc_medianizer=oracle_address).from_abi()
 
-feeder_rsk = RDOCPriceFeed(network_manager,
+feeder_roc = RDOCPriceFeed(network_manager,
                            contract_address=feeders[1][0],
                            contract_address_moc_medianizer=oracle_address).from_abi()
 
-range_blocks = [5285880, 5285888]
-block_skip = 3
+range_blocks = [5285307, 5285901]
+block_skip = 1
 
 display_table = []
 titles = ['blockNumber', 'Price Oracle', 'Price Feeder MOC', 'Price Feeder ROC', 'Timestamp']
+
+ora_prev = None
+moc_prev = None
+roc_prev = None
 
 for block_n in range(range_blocks[0], range_blocks[1], block_skip):
 
@@ -55,14 +60,35 @@ for block_n in range(range_blocks[0], range_blocks[1], block_skip):
     dt = ts - datetime.timedelta(hours=0)
     d_timestamp = dt.strftime("%Y-%m-%d %H:%M:%S")
 
+    ora = oracle.peek(block_identifier=block_n)[0]
+    moc = feeder_moc.peek(block_identifier=block_n)[0]
+    roc = feeder_roc.peek(block_identifier=block_n)[0]
+
+    if ora_prev != ora:
+        ora_prev = ora
+    else:
+        ora = None
+
+    if moc_prev != moc:
+        moc_prev = moc
+    else:
+        moc = None
+
+    if roc_prev != roc:
+        roc_prev = roc
+    else:
+        roc = None
+
     display_table.append([
         block_n,
-        oracle.peek(block_identifier=block_n)[0],
-        feeder_moc.peek(block_identifier=block_n)[0],
-        feeder_rsk.peek(block_identifier=block_n)[0],
+        ora,
+        moc,
+        roc,
         d_timestamp])
 
 print(tabulate(display_table, headers=titles, tablefmt="pipe"))
+with open('out.txt', 'w') as f:
+    print(tabulate(display_table, headers=titles, tablefmt="presto"), file=f)
 
 # finally disconnect from network
 network_manager.disconnect()
