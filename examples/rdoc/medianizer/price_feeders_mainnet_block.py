e89c84ca98228ff7b713f05b8446bc2c61377e6c
Juan S. Bokser <juan.bokser@gmail.com>
2023-05-09 21:20:21 -0300

    update price_feeders_mainnet_block.py

--- examples/rdoc/medianizer/price_feeders_mainnet_block.py
+++ examples/rdoc/medianizer/price_feeders_mainnet_block.py
@@ -9,27 +9,29 @@ from moneyonchain.networks import network_manager
 from moneyonchain.medianizer import RDOCMoCMedianizer, \
     RDOCPriceFeed
 
+# logging module
 import logging
 import logging.config
 
-# logging module
-# Initialize you log configuration using the base class
-logging.basicConfig(level=logging.INFO)
-# Retrieve the logger instance
-log = logging.getLogger()
-
-
-connection_network = 'rskMainnetPublic'
-config_network = 'rdocMainnet'
-
+# Some Params
+range_blocks = [5285307, 5286552]
 # connection network is the brownie connection network
+connection_network = 'rskMainnetPublic'
 # config network is our enviroment we want to connect
-network_manager.connect(connection_network=connection_network, config_network=config_network)
-
+config_network = 'rdocMainnet'
 
 oracle_address = '0x504EfCadFB020d6bBaeC8a5c5BB21453719d0E00'
 feeders = [('0x461750b4824b14c3d9b7702bC6fBB82469082b23', '# MOC'),
            ('0xBEd51D83CC4676660e3fc3819dfAD8238549B975', '# ROC')]
+
+
+# Initialize you log configuration using the base class
+logging.basicConfig(level=logging.INFO)
+
+# Retrieve the logger instance
+log = logging.getLogger()
+
+network_manager.connect(connection_network=connection_network, config_network=config_network)
 
 oracle = RDOCMoCMedianizer(network_manager,
                            contract_address=oracle_address).from_abi()
@@ -42,17 +44,14 @@ feeder_roc = RDOCPriceFeed(network_manager,
                            contract_address=feeders[1][0],
                            contract_address_moc_medianizer=oracle_address).from_abi()
 
-range_blocks = [5285307, 5285901]
-block_skip = 1
-
 display_table = []
-titles = ['blockNumber', 'Price Oracle', 'Price Feeder MOC', 'Price Feeder ROC', 'Timestamp']
+titles = ['Block', 'Oracle new price', 'Oracle value', 'Feeder MOC new price', 'Feeder MOC value', 'Feeder ROC new price', 'Feeder ROC value', 'Timestamp']
 
-ora_prev = None
-moc_prev = None
-roc_prev = None
+ora_prev = oracle.peek(block_identifier=range_blocks[0]-1)[0]
+moc_prev = feeder_moc.peek(block_identifier=range_blocks[0]-1)[0]
+roc_prev = feeder_roc.peek(block_identifier=range_blocks[0]-1)[0]
 
-for block_n in range(range_blocks[0], range_blocks[1], block_skip):
+for block_n in range(range_blocks[0], range_blocks[1], 1):
 
     print("Indexing Block {0} / {1}".format(block_n, range_blocks[1]))
 
@@ -81,14 +80,23 @@ for block_n in range(range_blocks[0], range_blocks[1], block_skip):
 
     display_table.append([
         block_n,
-        ora,
-        moc,
-        roc,
+        'X' if ora else '',
+        ora if ora else ora_prev,
+        'X' if moc else '',
+        moc if moc else moc_prev,
+        'X' if roc else '',
+        roc if roc else roc_prev,
         d_timestamp])
 
 print(tabulate(display_table, headers=titles, tablefmt="pipe"))
-with open('out.txt', 'w') as f:
-    print(tabulate(display_table, headers=titles, tablefmt="presto"), file=f)
+
+# csv output
+with open('price_feeders_mainnet_block.csv', 'w') as f:
+    fmt = lambda x: str(x).replace(',', '').replace('.', ',')
+    print(';'.join(map(fmt, titles)), file=f) 
+    for r in display_table:
+        print(';'.join(map(fmt, r)), file=f) 
+
 
 # finally disconnect from network
 network_manager.disconnect()
